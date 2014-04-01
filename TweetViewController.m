df168e958dc52ddadb6cd00b847931f5a263fdc9
Anthony Sherbondy <ts1235813@gmail.com>
2014-04-01 14:40:39 -0700

    full tweet view autolayout

--- TweetViewController.m
+++ TweetViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "TweetViewController.h"
+#import <UIImageView+AFNetworking.h>
 
 @interface TweetViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *authorNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *authorHandleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *authorImageView;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberRetweetsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberFavoritesLabel;
+@property (weak, nonatomic) IBOutlet UIButton *replyButton;
+@property (weak, nonatomic) IBOutlet UIButton *retweetButton;
+@property (weak, nonatomic) IBOutlet UIButton *favoriteButton;
 @end
 
 @implementation TweetViewController
@@ -27,15 +37,36 @@
 {
     [super viewDidLoad];
     
+    self.navigationItem.title = @"Tweet";
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Reply" style:UIBarButtonItemStylePlain target:self action:@selector(reply)];
     
     // update the ui based on the tweet
+    [self updateUIWithTweet];
 }
 
 - (void)setTweet:(Tweet *)tweet
 {
     // update the ui based on the tweet
     _tweet = tweet;
+    [self updateUIWithTweet];
+}
+
+- (void)updateUIWithTweet
+{
+    if (self.tweet) {
+        Tweet *tweet = self.tweet;
+        // Update the UI when we set the tweet
+        self.textLabel.text = tweet.text;
+        self.authorNameLabel.text = tweet.author.name;
+        self.authorHandleLabel.text = [NSString stringWithFormat:@"@%@", tweet.author.handle];
+        self.dateLabel.text = tweet.date.description;
+        [self.authorImageView setImageWithURL:[NSURL URLWithString:tweet.author.imageURL]];
+        
+        // Round the profile image corners
+        CALayer *layer = [self.authorImageView layer];
+        layer.masksToBounds = YES;
+        layer.cornerRadius = 10.0;
+    }
 }
 
 - (void)reply
