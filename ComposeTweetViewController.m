222505ac24e774dbd354ee6e18beee50c0c27ae9
Anthony Sherbondy <ts1235813@gmail.com>
2014-04-01 15:20:29 -0700

    compose view skeleton

--- ComposeTweetViewController.m
+++ ComposeTweetViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ComposeTweetViewController.h"
+#import "User.h"
+#import <UIImageView+AFNetworking.h>
 
 @interface ComposeTweetViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *userHandleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
 @end
 
 @implementation ComposeTweetViewController
@@ -29,6 +34,12 @@
     
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(cancel)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Tweet" style:UIBarButtonItemStylePlain target:self action:@selector(tweet)];
+    
+    // Update view
+    User *currentUser = [User currentUser];
+    self.userNameLabel.text = currentUser.name;
+    self.userHandleLabel.text = [NSString stringWithFormat:@"@%@",currentUser.handle];
+    [self.userImageView setImageWithURL:[NSURL URLWithString:currentUser.imageURL]];
 }
 
 - (void)cancel
